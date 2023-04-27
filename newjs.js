ed2c0252c0e3e0d91297f3f3c8e792773c92c341
Dhruv  Bhasin <dhruvbhasin0007@gmail.com>
2023-04-28 00:13:59 +0530

    new css updated

---
+++ newjs.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      prevArrow: '<button type="button" class="slick-prev">&#8592;</button>',
+      nextArrow: '<button type="button" class="slick-next">&#8594;</button>'
+    });
+  });
+  
